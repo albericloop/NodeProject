a8338d252a63a2fde26a1b70878ba9ec56ee8fb6
alberic dufort <alberic.dufort@gmail.com>
2018-11-15 15:20:04 +0100

    express

--- handles.js
+++ handles.js
@@ -1,37 +1,28 @@
 const http = require('http')
 const url = require('url')
-const querystring = require('querystring');
+const querystring = require('querystring')
+const express = require('express')
 
 module.exports = {
-  serverHandle: function(req, res) {
-  	const route = url.parse(req.url);
-  	const page = url.parse(req.url).pathname;
-  	const parameter = querystring.parse(route.query);
 
-  	if(page == "/"){
-	  	// Write a response header
-	  	res.writeHead(200, {'Content-Type': 'text/plain'});
+	app: app = express()
 
-	  	// Write a response content
-	  	res.end('You are on my first NodeJS web page! \n\nIf you want to know more about me, add /hello?name=Alberic to the URL! \n\nType /hello?name= with your name and a message will appear!');
-
-	}else if(page == "/hello" && 'name' in parameter){
-		//if the two conditions are ok
-	  	res.writeHead(200, {'Content-Type': 'text/plain'});
-
-	  	const name = parameter['name'];
-
-		if(name == "Alberic"){
-			//short introduction
-			res.end('Hello, I am Alberic Dufort, actually studying Big Data and Analytics at ECE Paris');
-		}else{
-			//hello name
-			res.end('hello ' + name);
-		}
-	}else{
-		//wrong path
-		res.writeHead(404);
-		res.end('Not found')
-	}
-  }
 }
+
+
+
+app.get('/', function (req, res) {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+	  	res.send('You are on my first NodeJS web page! \n\nIf you want to know more about me, add /hello?name=Alberic to the URL! \n\nType /hello?name= with your name and a message will appear!');
+})
+
+app.post('/', (req, res) => {
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+	  	res.send('You are on my first NodeJS web page! \n\nIf you want to know more about me, add /hello?name=Alberic to the URL! \n\nType /hello?name= with your name and a message will appear!');
+})
+
+app.get('/hello', function(req, res) {
+	res.send("Hello " + req.query.name)
+})
+
+
--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const http = require('http')
 const url = require('url')
 const handles = require('./handles')
+const express = require('express')
+//app = express()
 
-// Declare an http server
-http.createServer(handles.serverHandle).listen(8081)
+
+handles.app.set('port', 8081)
+
+app.set('views', __dirname + "/views")
+app.set('view engine', 'ejs');
+
+app.listen(
+  app.get('port'), 
+  () => console.log(`server listening on ${app.get('port')}`)
+)
